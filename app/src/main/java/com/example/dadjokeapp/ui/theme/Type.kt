e41919c786db23d3ab8d0fc3ca82afcff59c6c10
Herbert-Ken Ymera <hkymera2@gmail.com>
2025-06-01 14:53:27 +0300

    added fonts, improved spacing

--- app/src/main/java/com/example/dadjokeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/dadjokeapp/ui/theme/Type.kt
@@ -2,14 +2,23 @@ package com.example.dadjokeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.dadjokeapp.R
+
+val fontFamily = FontFamily(
+    Font(R.font.roboto_mono_variable_font, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.roboto_italic_variable_font, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.roboto_mono_bold, FontWeight.Bold,FontStyle.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
